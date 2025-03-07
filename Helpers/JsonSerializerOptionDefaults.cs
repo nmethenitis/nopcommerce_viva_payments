4ddf6317bc50d98b6423974a891d829abe20d90e
Nikolaos Methenitis <nmethenitis@hotmail.com>
2025-03-07 22:40:50 +0200

    Json settings

---
+++ Helpers/JsonSerializerOptionDefaults.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using AutoMapper.Execution;
+
+namespace Nop.Plugin.Payments.VivaPayments.Helpers;
+public static class JsonSerializerOptionDefaults {
+    public static JsonSerializerOptions GetDefaultSettings() {
+        var options = new JsonSerializerOptions {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            PropertyNameCaseInsensitive = true,
+            WriteIndented = false,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        options.Converters.Add(new JsonStringEnumConverter());
+        return options;
+    }
+}
